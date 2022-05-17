87f36f63ca4d3af0b6c7ab4373848927a5902264
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-17 16:18:41 +0200

    Added sensors to launchers

--- as2_launch.bash
+++ as2_launch.bash
@@ -6,14 +6,18 @@ drone_namespace=${AEROSTACK2_SIMULATION_DRONE_ID::-1}$N
 
 session=${USER}_$N
 
+sensor=$(ign topic -l | grep ${drone_namespace}/model | sed 's/\// /g' | awk '{print $2, $4, $6, $8, $10}' | sort -u | sed 's/ /,/g')
+sensor=$(echo $sensor | sed 's/ /:/g')
+
 # Create new session  (-2 allows 256 colors in the terminal, -s -> session name, -d -> not attach to the new session)
 tmux -2 new-session -d -s $session
 
 # Create roscore 
 # send-keys writes the string into the sesssion (-t -> target session , C-m -> press Enter Button)
-tmux new-window -t $session:0 -n 'ignition interface'
+tmux new-window -t $session:0 -n 'ignition_interface'
 tmux send-keys "ros2 launch ignition_platform ignition_platform_launch.py \
-    drone_id:=$drone_namespace " C-m
+    drone_id:=$drone_namespace \
+    sensors:=$sensor " C-m
 
 tmux new-window -t $session:1 -n 'controller_manager'
 tmux send-keys "ros2 launch controller_manager controller_manager_launch.py \
