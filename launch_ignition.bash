a0d47a1db0dc87896b409d7df1cd5da834f938e2
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-19 10:20:29 +0200

    Updated as2 var env

--- launch_ignition.bash
+++ launch_ignition.bash
@@ -5,5 +5,5 @@ config_path=${config_path:="config.json"}
 
 export RUN_ON_START=1
 
-SCRIPT_PATH="${AEROSTACK2_STACK}/simulation/ignition_assets/scripts"
+SCRIPT_PATH="${AEROSTACK2_PATH}/simulation/ignition_assets/scripts"
 $SCRIPT_PATH/run_ign.sh ${config_path}
