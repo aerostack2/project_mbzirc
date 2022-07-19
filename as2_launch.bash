2d20a400df0f9f0b1526d33a0d786c2ea193cd87
Pedro Arias <pedroariasperez96@gmail.com>
2022-07-19 11:48:26 +0200

    Update project with latest changes in as2 v020

--- as2_launch.bash
+++ as2_launch.bash
@@ -8,13 +8,16 @@ fi
 # Arguments
 drone_namespace=$1
 tree=$2  #TODO: temporal
-tree=${2:=""}
+tree=${tree:=""}
 
 source ./launch_tools.bash
 
 new_session $drone_namespace
 
 new_window 'ignition_interface' "ros2 launch ignition_platform ignition_platform_launch.py \
+    drone_id:=$drone_namespace"
+
+new_window 'state_estimator' "ros2 launch basic_state_estimator basic_state_estimator_launch.py \
     drone_id:=$drone_namespace"
 
 new_window 'controller_manager' "ros2 launch controller_manager controller_manager_launch.py \
@@ -24,9 +27,6 @@ new_window 'traj_generator' "ros2 launch trajectory_generator trajectory_generat
     drone_id:=$drone_namespace"
 
 new_window 'basic_behaviours' "ros2 launch as2_basic_behaviours all_basic_behaviours_launch.py \
-    drone_id:=$drone_namespace"
-
-new_window 'static_transform_publisher' "ros2 launch basic_tf_tree_generator basic_tf_tree_generator_launch.py \
     drone_id:=$drone_namespace"
 
 if [[ -n $tree ]]; then
--- stop.bash
+++ stop.bash
@@ -1,9 +1,4 @@
 #!/bin/bash
-SESSION=${AEROSTACK2_SIMULATION_DRONE_ID::-1}
+drone_namespace=${AEROSTACK2_SIMULATION_DRONE_ID::-1}
 
-sessions=$(tmux ls | awk '{print $1}' | sed "s/://g" | grep $SESSION)
-
-for sess in $sessions
-do
-    tmux kill-session -t $sess
-done
+tmux ls | grep -Po "${drone_namespace}\d+" | xargs -I % sh -c 'tmux kill-session -t %'
