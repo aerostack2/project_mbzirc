4d78b93289326cfdca9e2ec6d11fe7678c9b8d92
Miguel <miferco97@gmail.com>
2022-07-21 23:46:52 +0200

    namespace required for mission.py and as2_launch.bash

--- mbzirc_demo_1.bash
+++ mbzirc_demo_1.bash
@@ -6,10 +6,10 @@ drone_namespace=${AEROSTACK2_SIMULATION_DRONE_ID::-1}
 
 ${DIR_SCRIPT}/launch/as2_usv_launch.bash usv usv/tree.xml
 
-${DIR_SCRIPT}/launch/as2_launch.bash beacon_0 drone_roles/beacon.xml
-${DIR_SCRIPT}/launch/as2_launch.bash beacon_1 drone_roles/beacon.xml
-${DIR_SCRIPT}/launch/as2_launch.bash hunter_0 drone_roles/beacon.xml
-${DIR_SCRIPT}/launch/as2_launch.bash hunter_1 drone_roles/beacon.xml
+${DIR_SCRIPT}/as2_launch.bash beacon_0 drone_roles/beacon.xml
+${DIR_SCRIPT}/as2_launch.bash beacon_1 drone_roles/beacon.xml
+${DIR_SCRIPT}/as2_launch.bash hunter_0 drone_roles/beacon.xml
+${DIR_SCRIPT}/as2_launch.bash hunter_1 drone_roles/beacon.xml
 
 session=beacon_0
 
