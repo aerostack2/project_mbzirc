b87d8b2b48a6d526dc41fdbb3a5da324dbeb4b76
Pedro Arias <pedroariasperez96@gmail.com>
2022-08-22 20:20:15 +0200

    new launcher for dry_run v2

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -8,19 +8,24 @@ fi
 vehicle_name="$1"
 
 if [[ $vehicle_name == "usv" ]]; then
-    ./docker/as2_usv_launch.bash $vehicle_name
-elif [[ $vehicle_name =~ ^drone_[0-4]+$ ]]; then
-    ./docker/as2_drone_anchor.bash $vehicle_name
-# elif [[ $vehicle_name == "drone_2" ]]; then
-#     ${DIR_SCRIPT}/test.bash "Anchor 2"
-# elif [[ $vehicle_name == "drone_3" ]]; then
-#     ${DIR_SCRIPT}/test.bash "Anchor 3"
-# elif [[ $vehicle_name == "drone_4" ]]; then
-#     ${DIR_SCRIPT}/test.bash "Anchor 4"
+    # ./docker/as2_usv_launch.bash $vehicle_name
+    ros2 launch docker/usv.launch.py drone_id:=usv tree:=usv/usv.xml init_x:=-1462.0 init_y:=-16.5 init_z:=0.3
+# elif [[ $vehicle_name =~ ^drone_[0-4]+$ ]]; then
+    # ./docker/as2_drone_anchor.bash $vehicle_name
+elif [[ $vehicle_name == "drone_1" ]]; then
+    ros2 launch docker/anchor_sender.launch.py drone_id:=drone_1 tree:=drone_roles/anchor_drone_1.xml init_x:=-1476.0 init_y:=21.0 init_z:=4.18 
+elif [[ $vehicle_name == "drone_2" ]]; then
+    ros2 launch docker/anchor.launch.py drone_id:=drone_2 tree:=drone_roles/anchor_drone_2.xml init_x:=-1518.0 init_y:=-4.0 init_z:=4.18 
+elif [[ $vehicle_name == "drone_3" ]]; then
+    ros2 launch docker/anchor.launch.py drone_id:=drone_3 tree:=drone_roles/anchor_drone_3.xml init_x:=-1500.0 init_y:=-16.5 init_z:=4.18 
+elif [[ $vehicle_name == "drone_4" ]]; then
+    ros2 launch docker/anchor.launch.py drone_id:=drone_4 tree:=drone_roles/anchor_drone_4.xml init_x:=-1500.0 init_y:=-5.0 init_z:=4.18 
 elif [[ $vehicle_name == "drone_5" ]]; then
-    ./docker/as2_drone_inspection.bash $vehicle_name
+    # ./docker/as2_drone_inspection.bash $vehicle_name
+    ros2 launch docker/inspection.launch.py drone_id:=drone_5 tree:=drone_roles/anchor_drone_5.xml init_x:=-1462.0 init_y:=-17.7 init_z:=1.1
 elif [[ $vehicle_name == "drone_6" ]]; then
-    ./docker/as2_drone_inspection.bash $vehicle_name
+    # ./docker/as2_drone_inspection.bash $vehicle_name
+    ros2 launch docker/inspection.launch.py drone_id:=drone_6 tree:=drone_roles/anchor_drone_6.xml init_x:=-1462.0 init_y:=-15.3 init_z:=1.1
 # elif [[ $vehicle_name == "drone_7" ]]; then
 #     ${DIR_SCRIPT}/test.bash "Anchor 7"
 # elif [[ $vehicle_name == "drone_8" ]]; then
---
+++ docker/inspection.launch.py
@@ -0,0 +1,184 @@
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration
+
+
+def generate_launch_description():
+    drone_id = DeclareLaunchArgument('drone_id', default_value=[EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID'), 'drone_sim'])
+    controller_config = DeclareLaunchArgument('controller_config', default_value='robot_config/controller_manager.yaml')
+    takeoff_config = DeclareLaunchArgument('takeoff_config', default_value='robot_config/takeoff.yaml')
+
+    init_x = DeclareLaunchArgument('init_x', default_value='0.0')
+    init_y = DeclareLaunchArgument('init_y', default_value='0.0')
+    init_z = DeclareLaunchArgument('init_z', default_value='0.0')
+    
+    bt_tree = DeclareLaunchArgument('tree', default_value='')
+
+    camera_topic = DeclareLaunchArgument('camera_topic', default_value='stream/compressed_image')
+    report_topic = DeclareLaunchArgument('report_topic', default_value='report')
+
+    platform = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ignition_platform'), 'launch'),
+            '/ignition_platform_launch.py']),
+        launch_arguments={
+            'drone_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true'
+        }.items()
+    )
+
+    state_estimator = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('basic_state_estimator'), 'launch'),
+            '/basic_state_estimator_launch.py']),
+        launch_arguments={
+            'drone_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'ground_truth': 'false',
+            'odom_only': 'true',
+            'base_frame': '\"\"'
+        }.items()
+    )
+
+    vinodom = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('vinodom'), 'launch'),
+            '/vinodom_launch.py']),
+        launch_arguments={
+            'namespace': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'base_frame': LaunchConfiguration('drone_id'),
+            'show_matching': 'false',
+            'init_x': LaunchConfiguration('init_x'),
+            'init_y': LaunchConfiguration('init_y'),
+            'init_z': LaunchConfiguration('init_z')
+        }.items()
+    )
+
+    controller_manager = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('controller_manager'), 'launch'),
+            '/controller_manager_launch.py']),
+        launch_arguments={
+            'drone_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'config': LaunchConfiguration('controller_config')
+        }.items()
+    )
+
+    traj_generator = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('trajectory_generator'), 'launch'),
+            '/trajectory_generator_launch.py']),
+        launch_arguments={
+            'drone_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true'
+        }.items()
+    )
+
+    basic_behaviours = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('as2_basic_behaviours'), 'launch'),
+            '/all_basic_behaviours_launch.py']),
+        launch_arguments={
+            'drone_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'config_takeoff': LaunchConfiguration('takeoff_config')
+        }.items()
+    )
+
+    comms = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('mbzirc_comms'), 'launch'),
+            '/mbzirc_comms_launch.py']),
+        launch_arguments={
+            'robot_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'n_drones': '10',
+            'pose_topic': '/self_localization/pose',
+            'tree_topic': '/tree',
+            'camera_topic': LaunchConfiguration('camera_topic'),
+            'loc_hist_topic': '/loc_hist',
+            'report_topic': LaunchConfiguration('report_topic')
+        }.items()
+    )
+
+    planner = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('mbzirc_bt'), 'launch'),
+            '/mbzirc_bt.launch.py']),
+        launch_arguments={
+            'drone_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'tree':  LaunchConfiguration('tree'),
+            'groot_logger': 'false',
+            'server_timeout': '30000'
+        }.items()
+    )
+
+    # stream_sender = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(
+    #         get_package_share_directory('mbzirc_sim_interface'), 'launch'),
+    #         '/stream_sender_launch.py']),
+    #     launch_arguments={
+    #         'namespace': LaunchConfiguration('drone_id'),
+    #         'use_sim_time': 'true',
+    #         'compressed_image_topic': LaunchConfiguration('camera_topic'),
+    #         'report_topic': LaunchConfiguration('report_topic')
+    #     }.items()
+    # )
+
+    yolo_detector = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('yolo_object_detector'), 'launch'),
+            '/yolo_object_detector_launch.py']),
+        launch_arguments={
+            'drone_id': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'camera_topic': 'slot0/image_raw'
+        }.items()
+    )
+
+    stream_compressor = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('mbzirc_sim_interface'), 'launch'),
+            '/stream_compressor_launch.py']),
+        launch_arguments={
+            'namespace': LaunchConfiguration('drone_id'),
+            'use_sim_time': 'true',
+            'rgb_image_topic': 'slot0/image_raw',
+            'detection_topic': 'detector_node/detections',
+            'compressed_image_topic': '/drone_1/'+LaunchConfiguration('camera_topic'), # TODO: ojo, revisar si esto funciona
+            'report_topic': '/drone_1/'+LaunchConfiguration('report_topic')
+        }.items()
+    )
+
+    return LaunchDescription([
+        drone_id,
+        controller_config,
+        takeoff_config,
+        init_x,
+        init_y,
+        init_z,
+        bt_tree,
+        camera_topic,
+        report_topic,
+
+        platform,
+        state_estimator,
+        vinodom,
+        controller_manager,
+        traj_generator,
+        basic_behaviours,
+        comms,
+        planner,
+        yolo_detector,
+        stream_compressor
+    ])
