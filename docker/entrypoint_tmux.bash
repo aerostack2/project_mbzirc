b88dd4fe1038e064cdcab8014b9e63bbe04e8bbf
Pedro Arias <pedroariasperez96@gmail.com>
2022-08-23 12:19:51 +0200

    added initial pose

--- docker/entrypoint_tmux.bash
+++ docker/entrypoint_tmux.bash
@@ -8,19 +8,19 @@ fi
 vehicle_name="$1"
 
 if [[ $vehicle_name == "usv" ]]; then
-    ./docker/as2_usv_launch.bash $vehicle_name
-elif [[ $vehicle_name =~ ^drone_[0-4]+$ ]]; then
-    ./docker/as2_drone_anchor.bash $vehicle_name
-# elif [[ $vehicle_name == "drone_2" ]]; then
-#     ${DIR_SCRIPT}/test.bash "Anchor 2"
-# elif [[ $vehicle_name == "drone_3" ]]; then
-#     ${DIR_SCRIPT}/test.bash "Anchor 3"
-# elif [[ $vehicle_name == "drone_4" ]]; then
-#     ${DIR_SCRIPT}/test.bash "Anchor 4"
+    ./docker/as2_usv_launch.bash $vehicle_name -1462.0 -16.5 0.3
+elif [[ $vehicle_name == "drone_1" ]]; then
+    ./docker/as2_drone_anchor.bash $vehicle_name -1476.0 21.0 4.18 
+elif [[ $vehicle_name == "drone_2" ]]; then
+    ./docker/as2_drone_anchor.bash $vehicle_name -1518.0 -4.0 4.18
+elif [[ $vehicle_name == "drone_3" ]]; then
+    ./docker/as2_drone_anchor.bash $vehicle_name -1500.0 -16.5 4.18 
+elif [[ $vehicle_name == "drone_4" ]]; then
+    ./docker/as2_drone_anchor.bash $vehicle_name -1500.0 -5.0 4.18 
 elif [[ $vehicle_name == "drone_5" ]]; then
-    ./docker/as2_drone_inspection.bash $vehicle_name
+    ./docker/as2_drone_inspection.bash $vehicle_name -1462.0 -17.7 1.1
 elif [[ $vehicle_name == "drone_6" ]]; then
-    ./docker/as2_drone_inspection.bash $vehicle_name
+    ./docker/as2_drone_inspection.bash $vehicle_name -1462.0 -15.3 1.1
 # elif [[ $vehicle_name == "drone_7" ]]; then
 #     ${DIR_SCRIPT}/test.bash "Anchor 7"
 # elif [[ $vehicle_name == "drone_8" ]]; then
