37740910fe90b0ce5f257daff3f3a2f879899181
Pedro Arias <pedroariasperez96@gmail.com>
2022-08-30 12:24:21 +0200

    More tests to follow_path

--- test/follow_path/as2_launch.bash
+++ test/follow_path/as2_launch.bash
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-if [ "$#" -le 0 ]; then
-	echo "usage: $0 [drone_namespace] "
-	exit 1
-fi
-
 # Arguments
 drone_namespace=$1
+drone_namespace=${drone_namespace:='drone_1'}
 init_x=$2
+init_x=${init_x:=0.0}
 init_y=$3
+init_y=${init_y:=0.0}
 init_z=$4
+init_z=${init_z:=0.0}
 
 source ../../launch/launch_tools.bash
 
 new_session $drone_namespace  
 
-# new_window 'ignition_interface' "ros2 launch ignition_platform ignition_platform_launch.py \
-#     use_sim_time:=true \
-#     drone_id:=$drone_namespace"
+new_window 'ignition_interface' "ros2 launch ignition_platform ignition_platform_launch.py \
+    use_sim_time:=true \
+    drone_id:=$drone_namespace"
 
-# new_window 'state_estimator' "ros2 launch basic_state_estimator basic_state_estimator_launch.py \
-#     drone_id:=$drone_namespace \
-#     use_sim_time:=true \
-#     ground_truth:=false \
-#     odom_only:=true \
-#     base_frame:='\"\"'"
+new_window 'state_estimator' "ros2 launch basic_state_estimator basic_state_estimator_launch.py \
+    drone_id:=$drone_namespace \
+    use_sim_time:=true \
+    ground_truth:=true \
+    odom_only:=false \
+    base_frame:='\"\"'"
 
 # new_window 'visual_inertial_odometry' "ros2 launch vinodom vinodom_launch.py \
 #     namespace:=$drone_namespace \
@@ -35,24 +34,24 @@ new_session $drone_namespace
 #     init_y:=$init_y \
 #     init_z:=$init_z"
 
-# new_window 'controller_manager' "ros2 launch controller_manager controller_manager_launch.py \
-#     drone_id:=$drone_namespace \
-#     use_sim_time:=true \
-#     config:=robot_config/controller_manager.yaml"
+new_window 'controller_manager' "ros2 launch controller_manager controller_manager_launch.py \
+    drone_id:=$drone_namespace \
+    use_sim_time:=true \
+    config:=../../robot_config/controller_manager.yaml"
 
 new_window 'traj_generator' "ros2 launch trajectory_generator trajectory_generator_launch.py  \
-    use_sim_time:=true \
+    use_sim_time:=false \
     drone_id:=$drone_namespace"
 
-# new_window 'basic_behaviours' "ros2 launch as2_basic_behaviours all_basic_behaviours_launch.py \
-#     drone_id:=$drone_namespace  \
-#     use_sim_time:=true \
-#     config_takeoff:=../../robot_config/takeoff.yaml"
+new_window 'basic_behaviours' "ros2 launch as2_basic_behaviours all_basic_behaviours_launch.py \
+    drone_id:=$drone_namespace  \
+    use_sim_time:=true \
+    config_takeoff:=../../robot_config/takeoff.yaml"
 
 new_window 'mission_planner' "ros2 launch mbzirc_bt mbzirc_bt.launch.py \
     drone_id:=$drone_namespace \
     use_sim_time:=true \
-    tree:=./follow_path_test.xml \
+    tree:=./follow_path_test_real.xml \
     groot_logger:=true "
 
 echo -e "Launched drone $drone_namespace. For attaching to the session, run: \n  \t $ tmux a -t $drone_namespace"
@@ -65,3 +64,4 @@ function ctrl_c() {
 }
 
 # sleep 10000
+tmux a
--- test/follow_path/send_path.py
+++ test/follow_path/send_path.py
@@ -1,5 +1,7 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.clock import Clock
+from rclpy.time import Time
 
 from as2_msgs.msg import TrajectoryWaypointsWithID, PoseStampedWithID
 
@@ -15,34 +17,44 @@ class MinimalPublisher(Node):
 
     def timer_callback(self):
         msg = TrajectoryWaypointsWithID()
-        msg.yaw_mode = 2
+        msg.header.frame_id = "earth"
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.yaw_mode = TrajectoryWaypointsWithID.PATH_FACING
 
         pose = PoseStampedWithID()
+        pose.header.frame_id = "earth"
+        pose.header.stamp = self.get_clock().now().to_msg()
         pose.id = '0'
         pose.pose.position.x = 10.0
         pose.pose.position.y = 0.0
         pose.pose.position.z = 3.0
         msg.poses.append(pose)
 
-        pose.id = '1'
-        pose.pose.position.x = 0.0
-        pose.pose.position.y = 10.0
-        pose.pose.position.z = 3.0
-        msg.poses.append(pose)
+        pose1 = PoseStampedWithID()
+        pose1.header.frame_id = "earth"
+        pose1.header.stamp = self.get_clock().now().to_msg()
+        pose1.id = '1'
+        pose1.pose.position.x = 0.0
+        pose1.pose.position.y = 10.0
+        pose1.pose.position.z = 3.0
+        msg.poses.append(pose1)
 
-        pose.id = '2'
-        pose.pose.position.x = 10.0
-        pose.pose.position.y = 10.0
-        pose.pose.position.z = 3.0
-        msg.poses.append(pose)
+        pose2 = PoseStampedWithID()
+        pose2.id = '2'
+        pose2.pose.position.x = 10.0
+        pose2.pose.position.y = 10.0
+        pose2.pose.position.z = 3.0
+        msg.poses.append(pose2)
 
-        pose.id = '3'
-        pose.pose.position.x = 0.0
-        pose.pose.position.y = 0.0
-        pose.pose.position.z = 3.0
-        msg.poses.append(pose)
+        pose3 = PoseStampedWithID()
+        pose3.id = '3'
+        pose3.pose.position.x = 0.0
+        pose3.pose.position.y = 0.0
+        pose3.pose.position.z = 3.0
+        msg.poses.append(pose3)
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.poses)
+        self.get_logger().info('Publishing: "%s"' % msg)
         self.i += 1
 
 
