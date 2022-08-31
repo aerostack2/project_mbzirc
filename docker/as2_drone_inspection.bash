fb2a2181f782a9db5baba1d5e3a48226da10038c
Pedro Arias <pedroariasperez96@gmail.com>
2022-08-31 21:53:00 +0200

    update inspection launch

--- docker/as2_drone_inspection.bash
+++ docker/as2_drone_inspection.bash
@@ -75,7 +75,7 @@ new_window 'comms' "ros2 launch mbzirc_comms mbzirc_comms_launch.py \
 new_window 'mission_planner' "ros2 launch mbzirc_bt mbzirc_bt.launch.py \
     drone_id:=$drone_namespace \
     use_sim_time:=true \
-    tree:=drone_roles/anchor_$drone_namespace.xml \
+    tree:=drone_roles/inspection_$drone_namespace.xml \
     groot_logger:=false \
     server_timeout:=30000 "
 
